0bf3a63ab404800d26b9ab20d39a8dbf41aba872
bedroesb <bedro@psb.vib-ugent.be>
2022-11-30 18:08:43 +0100

    working script

--- var/title_updater.py
+++ var/title_updater.py
@@ -17,23 +17,28 @@ mapping_path = 'faircookbook_rdmkit_mapping.yml'
 
 # FAIRCookbook
 with open(fcb_cache_path, 'r') as fcb_cache:
-    fcb_cached_content = yaml.load(fcb_cache, Loader=yaml.FullLoader)
+    fcb_cached_content = yaml.load(fcb_cache)
 
 # RDMkit
 with open(rdmkit_cache_path, 'r') as rdmkit_cache:
-    rdmkit_cached_content = yaml.load(rdmkit_cache, Loader=yaml.FullLoader)
+    rdmkit_cached_content = yaml.load(rdmkit_cache)
 
 # ---- Parsing mapping file ----
 # RDMkit
 with open(mapping_path, 'r') as mapping_file:
-    mapping_dict = yaml.load(mapping_file, Loader=yaml.FullLoader)
+    mapping_dict = yaml.load(mapping_file)
 
 # ---- Updating mapping dict with titles from respective cache files ----
 
-for rdmkit_page in mapping_dict:
+for i,rdmkit_page in enumerate(mapping_dict):
     if rdmkit_page['rdmkit_filename'] in rdmkit_cached_content:
-        rdmkit_page['rdmkit_title'] = rdmkit_cached_content[rdmkit_page['rdmkit_filename']]
+        mapping_dict[i]['rdmkit_title'] = rdmkit_cached_content[rdmkit_page['rdmkit_filename']]
     if 'links' in rdmkit_page and rdmkit_page['links']:
-        for fcb_recipe in rdmkit_page['links']:
+        for j,fcb_recipe in enumerate(rdmkit_page['links']):
             if fcb_recipe['fcb_id'] in fcb_cached_content:
-                fcb_recipe['fcb_title'] = fcb_cached_content[fcb_recipe['fcb_id']]
+                mapping_dict[i]['links'][j]['fcb_title'] = fcb_cached_content[fcb_recipe['fcb_id']]
+
+# ---- Dump changes to file ----
+
+with open(mapping_path, 'w') as mapping_file:
+    yaml.dump(mapping_dict, mapping_file)
